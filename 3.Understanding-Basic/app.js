c820e18e94d00ccada2cb63fa81a696a79d279cc
muktidj <muktidwijatmoko@gmail.com>
2019-12-06 21:26:56 +0700

    bab 3 3.Membuat Response dan memberikan response tentang html ditulis dan mengatur Content-Type nya juga

--- 3.Understanding-Basic/app.js
+++ 3.Understanding-Basic/app.js
@@ -1,8 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  console.log(request);
-  //process.exit(); //Mematikan server
+  console.log(request.url, request.method, request.headers);
+
+//   response
+response.setHeader('Content-Type', 'text/html')
+
+response.write('<html>')
+response.write('<head><title>test-node</title></head>')
+response.write('<body><h1>Hello From Node</h1></body>')
+response.write('</html>')
+response.end()
+
 });
 
 server.listen(3000);
