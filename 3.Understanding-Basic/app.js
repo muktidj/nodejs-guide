2c7e1cc049cde8439c65c19b1d684ae4bf5eceb2
muktidj <muktidwijatmoko@gmail.com>
2019-12-06 21:45:03 +0700

    bab 3 4.Membuat Request Routing dengan node js (node jse bukan express ya)

--- 3.Understanding-Basic/app.js
+++ 3.Understanding-Basic/app.js
@@ -1,17 +1,24 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  console.log(request.url, request.method, request.headers);
+  //Routing Request
+  const url = request.url;
+  if (url === "/") {
+    response.write("<html>");
+    response.write("<head><title>Send Message</title></head>");
+    response.write(
+      '<body><form action="/message" method="POST"><input type="text" name"message"><button type="submit">send</button></form></body>'
+    );
+    response.write("</html>");
+    return response.end; 
+  }
+  response.setHeader("Content-Type", "text/html");
 
-//   response
-response.setHeader('Content-Type', 'text/html')
-
-response.write('<html>')
-response.write('<head><title>test-node</title></head>')
-response.write('<body><h1>Hello From Node</h1></body>')
-response.write('</html>')
-response.end()
-
+  response.write("<html>");
+  response.write("<head><title>test-node</title></head>");
+  response.write("<body><h1>Hello From Node</h1></body>");
+  response.write("</html>");
+  response.end();
 });
 
 server.listen(3000);
