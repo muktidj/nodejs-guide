7cea54e061b4fc6f673d429d4a41dd38117037de
muktidj <muktidwijatmoko@gmail.com>
2019-12-06 23:27:59 +0700

    bab 3. 6. Mencoba Parsing Request Bodies menggunakan buffer

--- 3.Understanding-Basic/app.js
+++ 3.Understanding-Basic/app.js
@@ -1,31 +1,39 @@
 const http = require("http");
 const fs = require("fs");
 
-const server = http.createServer((request, response) => {
-  //Routing Request
-  const url = request.url;
-  const method = request.method;
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
   if (url === "/") {
-    response.write("<html>");
-    response.write("<head><title>Send Message</title></head>");
-    response.write(
-      '<body><form action="/message" method="POST"><input type="text" name"message"><button type="submit">send</button></form></body>'
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
-    response.write("</html>");
-    return response.end;
+    res.write("</html>");
+    return res.end();
   }
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "Ini pesan dari message.txt");
-    response.statusCode = 302;
-    response.setHeader("Location", "/");
-    return response.end();
+    const body = [];
+    req.on("data", chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   }
-  response.setHeader("Content-Type", "text/html");
-  response.write("<html>");
-  response.write("<head><title>test-node</title></head>");
-  response.write("<body><h1>Hello From Node</h1></body>");
-  response.write("</html>");
-  response.end();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
