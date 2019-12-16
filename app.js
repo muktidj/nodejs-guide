0080ceeaa2e3fa93f036a99880f9e8f35667a9a6
muktidj <muktidwijatmoko@gmail.com>
2019-12-16 15:18:42 +0700

    BAB 5 5. Handling different route with middleware

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Run 1");
-  next(); // Mengizinkan permintaan selanjutnya
+app.use("/", (req, res, next) => {
+  console.log("Always Run");
+  next();
 });
-app.use((req, res, next) => {
+
+app.use("/add", (req, res, next) => {
+  console.log("Run 1");
+  res.send("<h1>I Like Sex</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("Kiss Me");
+
   res.send("<h1>Hello World, From Express</h1>");
 });
 
