2a520d8421c212ddb395c2c170f6935629e67f94
muktidj <muktidwijatmoko@gmail.com>
2019-12-16 14:49:19 +0700

    BAB 5 4. Express dibelakang layar, menghapusr variabel server

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const http = require("http");
-
 const express = require("express");
 const app = express();
-const server = http.createServer(app);
 
 app.use((req, res, next) => {
   console.log("Run 1");
@@ -12,4 +9,4 @@ app.use((req, res, next) => {
   res.send("<h1>Hello World, From Express</h1>");
 });
 
-server.listen(8000);
+app.listen(8000);
