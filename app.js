0dc3860afa2f05f314e5f40a4b2a01976a945876
muktidj <muktidwijatmoko@gmail.com>
2019-12-18 11:29:23 +0700

    BAB 5 8. Merouting dari folder routes menggunakan express routing

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-  res.redirect("/");
-});
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
-});
+
+
 app.listen(8000);
