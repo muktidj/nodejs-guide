3040b186b79538054531cc94e28d63ea6178281d
muktidj <muktidwijatmoko@gmail.com>
2019-12-21 21:36:51 +0700

    Bab 6 1. Sharing lintas data & user pada routes/ dan app.js

--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,12 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
+const adminRoutes = require('./admin')
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  console.log('shop.js', adminRoutes.products)
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
