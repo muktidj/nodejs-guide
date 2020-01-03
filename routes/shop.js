c63f92c1a8a32ec384dbc5bf42c17bd0f765eeb6
muktidj <muktidwijatmoko@gmail.com>
2020-01-03 11:19:51 +0700

    Bab 7 1. Membuat folder controllers-products.js, mengubah folder routes untuk jd konsep controller, mengedit app.js

--- routes/shop.js
+++ routes/shop.js
@@ -2,21 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+// const rootDir = require('../util/path');
+// rootDir Sudah tidak digunakan lagi jika menggunakan konsep MVC
+
+
+const productsController = require('../controllers/products')
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get('/', productsController.getProduct);
 
 module.exports = router;
