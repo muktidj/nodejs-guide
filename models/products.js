8da3c60a395b9befa6574bd3644bf290b43c145b
muktidj <muktidwijatmoko@gmail.com>
2020-01-03 14:50:35 +0700

    Bab 7 6.Refactor Code

--- models/products.js
+++ models/products.js
@@ -1,22 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 
+const callPath = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+  );
+
+  const getProductsFromFile = callBack => {
+    fs.readFile(callPath, (err, fileContent) => {
+      if (err) {
+        callBack([]);
+      } else {
+        callBack(JSON.parse(fileContent));
+      }
+    });
+  };
+
 module.exports = class Product {
   constructor(callTitle) {
     this.title = callTitle;
   }
-
   save() {
-    const callPath = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(callPath, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(callPath, JSON.stringify(products), err => {
         console.log(err);
@@ -25,16 +31,6 @@ module.exports = class Product {
   }
 
   static fetchAll(callBack) {
-    const callPath = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(callPath, (err, fileContent) => {
-      if (err) {
-       callBack([]);
-      }
-      callBack(JSON.parse(fileContent));
-    });
+    getProductsFromFile(callBack);
   }
 };
