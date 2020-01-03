065bcf6aa2fdbf3c30738ce245f105f92c2fd6f6
muktidj <muktidwijatmoko@gmail.com>
2020-01-03 14:30:37 +0700

    Bab 7 4.Menyimpan data difile data/product.json

--- models/products.js
+++ models/products.js
@@ -1,16 +1,40 @@
-const products = []
-
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
-    constructor(callTitle) {
-        this.title = callTitle
-    }
+  constructor(callTitle) {
+    this.title = callTitle;
+  }
 
-    save() {
-        products.push(this)
-    }
+  save() {
+    const callPath = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(callPath, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(callPath, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll() {
-        return products
-    }
-}
+  static fetchAll(callBack) {
+    const callPath = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(callPath, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
+  }
+};
