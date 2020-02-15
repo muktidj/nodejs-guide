e735830a118b4414f66b88bca45b653101cd6319
muktidj <muktidwijatmoko@gmail.com>
2020-02-15 19:50:29 +0700

    Bab 11 11.Creating Managing User && Dummy

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const errorController = require("./controllers/error");
 const sequalize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart-Item");
 
 const app = express();
 
@@ -20,6 +21,13 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req,res,next) => {
+  User.findByPk(1).then(user => {
+    req.user = user
+    next()
+  }).catch(err => console.log(err))
+})
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -27,13 +35,30 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
 User.hasMany(Product)
+// User.hasOne(Cart)
+// Cart.belongsTo(User)
+// Cart.belongsToMany(Product, {through: CartItem})
+// Product.belongsToMany(Cart, {through: CartItem})
+
 
 sequalize
-  .sync({force: true})
+  .sync()
   .then(result => {
     //console.log(result);
-    app.listen(3000);
+    return User.findByPk(1)
+
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({name: 'MuktiDJ', email: 'muktidwijatmoko@gmail.com'})
+    }
+    return user;
+  })
+  .then(user => {
+    console.log(user);
+    app.listen(9000);
+
   })
   .catch(err => {
-    //console.log(err);
+    console.log(err);
   });
